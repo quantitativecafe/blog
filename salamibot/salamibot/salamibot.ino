073c674fce665b43ddb818a6742e01b3f241518e
Michael Cooper <crwper@gmail.com>
2022-04-02 16:40:39 -0600

    Add maximum sample time

--- salamibot/salamibot/salamibot.ino
+++ salamibot/salamibot/salamibot.ino
@@ -2,9 +2,11 @@
 
 static char command[20];
 static bool is_running;
+static bool is_flowing;
 static int cur_pos;
 
-static unsigned long ms_zero, ms_print;
+static unsigned long ms_zero, ms_print, ms_start_flow;
+static unsigned long ms_trans, ms_max;
       
 static enum {
   STATE_IDLE,
@@ -57,19 +59,22 @@ void handle_command(char *str) {
     init_stepper();
 
     // Display status
-    Serial.println("Ready...");
+    Serial.println("Press space to start timer...");
     
     // Start manual triggering
     state = STATE_MANUAL;
   } else if (!strncmp(tokens[0], "e", 1)) {
+    // Save maximum sample time
+    ms_max = 3000;
+
     // Start in inactive mode
-    is_running = false;
+    is_running = is_flowing = false;
     
     // Initialize stepper motor
     init_stepper();
 
     // Display status
-    Serial.println("Ready...");
+    Serial.println("Press space to start timer...");
     
     // Start exponential triggering
     state = STATE_EXP;
@@ -134,9 +139,16 @@ void handle_input(char ch) {
         ms_start = ms_end = ms_zero = millis();
         ms_print = 0;
 
+        // Display status
+        Serial.println("Press space to advance to next tube...");
+        Serial.println("Press escape to finish...");
+
         // Change to active mode
         is_running = true;
       }
+
+      // Remember transition time
+      ms_trans = (ms_start + ms_end) / 2;
       
       // Print time
       Serial.print(ms_start - ms_zero);
@@ -168,20 +180,53 @@ void handle_input(char ch) {
     } else if (ch == ' ') {
       unsigned long ms_start, ms_end;
       
-      if (!is_running) {
+      if (is_flowing) {
+        // Increment position
+        ++cur_pos;
+      
+        // Go to new position
+        ms_start = millis();
+        stepper.runToNewPosition(10 * cur_pos);
+        ms_end = millis();
+
+        // Display status
+        Serial.println("Changed to manual mode...");
+        Serial.println("Press space to advance to next tube...");
+
+        // Go to manual state
+        state = STATE_MANUAL;
+      } else if (is_running) {
         // Initialize timers
-        ms_start = ms_end = ms_zero = millis();
+        ms_start = ms_end = ms_start_flow = millis();
+
+        // Display status
+        Serial.println("Press space to change to manual mode...");
+        Serial.println("Press escape to finish...");
+
+        // Change mode to flowing
+        is_flowing = true;
+      } else {
+        // Initialize timers
+        ms_zero = millis();
         ms_print = 0;
 
-        // Change to active mode
+        // Display status
+        Serial.println("Press space when flow starts...");
+
+        // Change mode to running
         is_running = true;
       }
-      
-      // Print time
-      Serial.print(ms_start - ms_zero);
-      Serial.print(",");
-      Serial.print(ms_end - ms_zero);
-      Serial.print("\r\n");
+
+      if (is_flowing) {
+        // Remember transition time
+        ms_trans = (ms_start + ms_end) / 2;
+
+        // Print time
+        Serial.print(ms_start - ms_zero);
+        Serial.print(",");
+        Serial.print(ms_end - ms_zero);
+        Serial.print("\r\n");
+      }
     }
   }
 }
@@ -205,16 +250,15 @@ void loop() {
     handle_input(ch);
   }
 
-  if (state == STATE_EXP && is_running) {
+  if ((state == STATE_EXP) && is_flowing) {
     // Get current time
     unsigned long ms = millis();
 
     const long N = 20;
     const float tau = 15000;
   
-    int next_pos = N - (int) ceil(N * exp(-(float) (ms - ms_zero) / tau));
-    if (next_pos > cur_pos)
-    {
+    int next_pos = N - (int) ceil(N * exp(-(float) (ms - ms_start_flow) / tau));
+    if (next_pos > cur_pos) {
       // Increment position
       cur_pos = next_pos;
     
@@ -223,6 +267,40 @@ void loop() {
       stepper.runToNewPosition(10 * cur_pos);
       unsigned long ms_end = millis();
     
+      // Remember transition time
+      ms_trans = (ms_start + ms_end) / 2;
+
+      // Print time
+      Serial.print(ms_start - ms_zero);
+      Serial.print(",");
+      Serial.print(ms_end - ms_zero);
+      Serial.print("\r\n");
+    } else if ((ms - ms_trans > ms_max) && is_flowing) {
+      // Display status
+      Serial.println("Changed to manual mode...");
+      Serial.println("Press space to advance to next tube...");
+
+      // Go to manual state
+      state = STATE_MANUAL;
+    }
+  }
+
+  if ((state == STATE_MANUAL) && is_running) {
+    // Get current time
+    unsigned long ms = millis();
+
+    if (ms - ms_trans > ms_max) {
+      // Increment position
+      ++cur_pos;
+    
+      // Go to new position
+      unsigned long ms_start = millis();
+      stepper.runToNewPosition(10 * cur_pos);
+      unsigned long ms_end = millis();
+    
+      // Remember transition time
+      ms_trans = (ms_start + ms_end) / 2;
+
       // Print time
       Serial.print(ms_start - ms_zero);
       Serial.print(",");
@@ -231,7 +309,7 @@ void loop() {
     }
   }
 
-  if (state != STATE_IDLE && is_running) {
+  if ((state != STATE_IDLE) && is_running) {
     // Get current time
     unsigned long ms = millis();
 
